d74b96201b6e0afba1e5d67c83d779e839674bf7
Ismael Jaouhar <ismael.jaouhar@gmail.com>
2019-02-20 15:13:41 +0100

    To it2 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FoodBox from './components/FoodBox';
+import 'bulma/css/bulma.css';
+import foods from './data/foods.json';
 
 class App extends Component {
+  state = {
+    foods: foods,
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          {this.state.foods.map((food, i) => 
+          <FoodBox 
+          key={i} 
+          name={food.name} 
+          cal={food.calories} 
+          imgUrl={food.image}
+          />
+          
+          )}
+        
+        </div>
       </div>
     );
   }
